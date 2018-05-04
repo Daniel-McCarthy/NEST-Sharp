0289d9a5ff17972edadaec5fa303f7c184c10897
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-03 20:09:36 -0500

    NEST: Created Rom Class
    This is class will contain and assess rom data for use with the
    emulator. It will be used to load into memory.

---
+++ Source/NEST/Classes/Rom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NEST.Classes
+{
+
+    class Rom
+    {
+    }
+}
