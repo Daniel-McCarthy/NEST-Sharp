158a56db69570a7353d3955da54dcb1f2f5062a6
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-03 20:13:11 -0500

    Rom Class: Added Function that checks for INES header
    If the rom file is long enough to contain an INES header it will check
    the first 4 bytes to make sure that it is the correct INES file
    signature. It will return true or false.

--- Source/NEST/Classes/Rom.cs
+++ Source/NEST/Classes/Rom.cs
@@ -15,6 +15,19 @@ namespace NEST.Classes
         public Rom(byte[] fileData)
         {
             romData = fileData;
+
+        bool checkForINESFormat()
+        {
+            // Checks first for file bytes for "NES" and MS-DOS eof signature.
+
+            if (romData != null && romData.Length >= 4)
+            {
+                return ((romData[0] << 24) | (romData[1] << 16) | (romData[2] << 8) | romData[3]) == 0x4E45531A;
+            }
+
+            return false;
+        }
+
         }
     }
 }
