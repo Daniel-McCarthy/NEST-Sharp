a3d99e07f8aa6dff5749007d6b68365bb4fad9d9
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-03 20:27:21 -0500

    NEST: Added Memory Class
    Class which contains and accesses cpu and ppu ram maps.

---
+++ Source/NEST/Classes/Memory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NEST.Classes
+{
+    class Memory
+    {
+        private byte[] cpuRam = new byte[0x10000];
+        private byte[] ppuRam = new byte[0x4000];
+
+
+    }
+}
