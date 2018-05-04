0dd28d7d0a32a86c7741afb34c8b79bbf6eba460
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-03 20:22:32 -0500

    NEST: Added CPU Class
    -Added initial class
    -Added register cpu fields

---
+++ Source/NEST/Classes/CPU.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEST.Classes
+{
+    class CPU
+    {
+        //NEST CPU Registers
+        private byte accumulator = 0;
+        private byte xAddress = 0;
+        private byte yAddress = 0;
+        public ushort programCounter = 0;
+        private byte stackPointer = 0;
+        private byte statusRegister = 0;
+
+
+    }
+}
