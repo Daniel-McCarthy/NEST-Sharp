87ba73a58eb8a61720c6b5a224aa3ac0f2793edf
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-05 17:34:12 -0500

    Moved ppuRam to PPU class, removed Memory class
    Memory is not one contained unit for the NES. Therefore it doesn't make
    sense to jam the ppu ram and the cpu ram into a different class. So
    instead I am keeping them in their appropriate classes.

--- Source/NEST/Classes/PPU.cs
+++ Source/NEST/Classes/PPU.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Windows.Forms;
 
 namespace NEST.Classes
 {
-    class Memory
+    class PPU
     {
         private byte[] ppuRam = new byte[0x4000];
-
-
     }
 }
