0f06db75a51e9dde5acb882afbe6936a28491f0e
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-05 19:55:46 -0500

    CPU: Added Xor A 0x49 Instruction Implementation

--- Source/NEST/Classes/CPU.cs
+++ Source/NEST/Classes/CPU.cs
@@ -327,6 +327,19 @@ namespace NEST.Classes
             tClock += 4;
         }
 
+        private void opcode49()
+        {
+            //Bitwise XOR A with Immediate byte
+
+            accumulator = (byte)(accumulator ^ readImmediateByte());
+
+            setFlagTo(Zero_Flag, (accumulator == 0));
+            setFlagTo(Negative_Flag, (accumulator & 0x80) != 0);
+
+            mClock += 1;
+            tClock += 4;
+        }
+
 
         /*
          * @Name: setFlagTo
