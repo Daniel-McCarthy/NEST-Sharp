f77d987031f69ff607dfba25f048dc5bade5fd5c
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-14 16:43:56 -0500

    CPU: Added ADC 0x7D Instruction Implementation

--- Source/NEST/Classes/CPU.cs
+++ Source/NEST/Classes/CPU.cs
@@ -1297,6 +1297,25 @@ namespace NEST.Classes
             setFlagTo(Negative_Flag, (accumulator & 0x80) == 0x80);
         }
 
+        private void opcode7D()
+        {
+            //ADC: Add Byte at absolute + X address + Carry Flag and copy it to Accumulator
+
+            int originalValue = accumulator;
+            ushort address = readImmediateUShort();
+            address += xAddress;
+            int additionByte = readCPURam(address);
+            int carryAmmount = getFlagStatus(Carry_Flag) ? 1 : 0;
+            int sum = (byte)(originalValue + additionByte + Carry_Flag);
+
+            accumulator = (byte)(sum & 0xFF);
+
+            setFlagTo(Overflow_Flag, detectOverflow(originalValue, additionByte, sum));
+            setFlagTo(Carry_Flag, sum > 0xFF);
+            setFlagTo(Zero_Flag, accumulator == 0);
+            setFlagTo(Negative_Flag, (accumulator & 0x80) == 0x80);
+        }
+
         private void opcode7E()
         {
             //Bitwise Right Rotate of value at absolute X address
