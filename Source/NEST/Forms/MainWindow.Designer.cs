86a481a757cb4f30e047f3a08c3b985e2e9b816b
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-15 10:06:19 -0500

    Added MainWindow Form

---
+++ Source/NEST/Forms/MainWindow.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace NEST
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resumeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.debuggerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.assemblyViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.memoryViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.statusToolStripMenuItem,
+            this.debuggerToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(256, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.settingsToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem1
+            // 
+            this.settingsToolStripMenuItem1.Name = "settingsToolStripMenuItem1";
+            this.settingsToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem1.Text = "Settings";
+            // 
+            // statusToolStripMenuItem
+            // 
+            this.statusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resumeToolStripMenuItem,
+            this.pauseToolStripMenuItem});
+            this.statusToolStripMenuItem.Name = "statusToolStripMenuItem";
+            this.statusToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.statusToolStripMenuItem.Text = "Status";
+            // 
+            // resumeToolStripMenuItem
+            // 
+            this.resumeToolStripMenuItem.Enabled = false;
+            this.resumeToolStripMenuItem.Name = "resumeToolStripMenuItem";
+            this.resumeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.resumeToolStripMenuItem.Text = "Resume";
+            this.resumeToolStripMenuItem.Click += new System.EventHandler(this.resumeToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Enabled = false;
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pauseToolStripMenuItem_Click);
+            // 
+            // debuggerToolStripMenuItem
+            // 
+            this.debuggerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.assemblyViewToolStripMenuItem,
+            this.memoryViewToolStripMenuItem});
+            this.debuggerToolStripMenuItem.Name = "debuggerToolStripMenuItem";
+            this.debuggerToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.debuggerToolStripMenuItem.Text = "Debugger";
+            // 
+            // assemblyViewToolStripMenuItem
+            // 
+            this.assemblyViewToolStripMenuItem.Name = "assemblyViewToolStripMenuItem";
+            this.assemblyViewToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.assemblyViewToolStripMenuItem.Text = "Assembly View";
+            this.assemblyViewToolStripMenuItem.Click += new System.EventHandler(this.assemblyViewToolStripMenuItem_Click);
+            // 
+            // memoryViewToolStripMenuItem
+            // 
+            this.memoryViewToolStripMenuItem.Name = "memoryViewToolStripMenuItem";
+            this.memoryViewToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.memoryViewToolStripMenuItem.Text = "Memory View";
+            this.memoryViewToolStripMenuItem.Click += new System.EventHandler(this.memoryViewToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(256, 266);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "NEST";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ToolStripMenuItem debuggerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem assemblyViewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem memoryViewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem statusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resumeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+    }
+}
+
---
+++ Source/NEST/Forms/MainWindow.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+using Core = NEST.Classes.Core;
+
+using System.Threading;
+using NEST.Classes;
+using NEST.Classes.Mappers;
+
+namespace NEST
+{
+    public partial class MainWindow : Form
+    {
+        private Thread emulatorThread;
+
+        private AssemblyView assemblyView;
+        private MemoryView memoryView;
+
+        public Classes.Canvas drawCanvas = new Classes.Canvas();
+        public SFML.Graphics.Sprite frame;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Add drawing canvas to screen
+            Controls.Add(drawCanvas);
+            drawCanvas.Location = new Point(0, 27);
+
+        }
+
+        
+        /*
+         *  Load Rom File 
+        */
+        
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            Core.paused = true;
+            string filePath = (openFileDialog1.ShowDialog() == DialogResult.OK) ? openFileDialog1.FileName : "Error: No such file found.";
+
+            if(File.Exists(filePath))
+            {
+
+                Core.cpu.programCounter = 0x8000;
+
+                Core.rom = new Rom(File.ReadAllBytes(filePath));
+
+                int mapperSetting = Core.rom.getMapperSetting();
+
+                if(mapperSetting == 0)
+                {
+                    NROM.loadRom(Core.rom);
+                }
+
+                //Core.beakMemory.initializeGameBoyValues();
+                //Core.beakMemory.readRomHeader();
+
+                if (emulatorThread == null)
+                {
+                    emulatorThread = new Thread(global::NEST.Classes.NEST_Main.startEmulator);
+                    emulatorThread.Start();
+                }
+
+                resumeToolStripMenuItem.Enabled = true;
+                pauseToolStripMenuItem.Enabled = true;
+
+            }
+            else
+            {
+                MessageBox.Show(filePath);
+                //Core.beakMemory.romFilePath = "";
+            }
+        }
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            //Tell the thread to stop when it finishes it's current loop
+            Core.run = false;
+
+            int loops = 0;
+            while(emulatorThread.IsAlive)
+            {
+                //Attempt to wait for the thread to be ready to be stopped.
+                if(emulatorThread.ThreadState == ThreadState.WaitSleepJoin)
+                {
+                    Thread.Sleep(800);
+                    emulatorThread.Abort();
+                }
+
+                //If the thread is not exiting after several attempts to check, just end it.
+                if (loops++ > 20)
+                {
+                    emulatorThread.Abort();
+                }
+
+                //Wait before trying again
+                Thread.Sleep(100);
+
+            }
+        }
+
+        private void assemblyViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (assemblyView == null || assemblyView.IsDisposed)
+            {
+                assemblyView = new AssemblyView();
+                assemblyView.Show();
+            }
+            else
+            {
+                assemblyView.Show();
+                assemblyView.BringToFront();
+            }
+        }
+
+        private void memoryViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (memoryView == null || memoryView.IsDisposed)
+            {
+                memoryView = new MemoryView();
+                memoryView.Show();
+            }
+            else
+            {
+                memoryView.Show();
+                memoryView.BringToFront();
+            }
+        }
+
+        private void resumeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Core.paused = false;
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Core.paused = true;
+        }
+    }
+
+}
