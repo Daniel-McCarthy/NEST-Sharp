7f6b1069d0e1a0c9de5f8eab4cbb4fcbc5961952
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-16 18:11:35 -0500

    Added BreakpointInputBox Form
    This inherits from the general use Input Box form. It's purpose is to
    accept hexadecimal addresses to be used to add as a breakpoint.

---
+++ Source/NEST/Forms/BreakpointInputBox.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace NEST.Forms
+{
+    partial class BreakpointInputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BreakpointInputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(303, 79);
+            this.Name = "BreakpointInputBox";
+            this.Text = "Add Breakpoint";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BreakpointInputBox_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Source/NEST/Forms/BreakpointInputBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NEST.Forms
+{
+    public partial class BreakpointInputBox : NEST.Forms.InputBox
+    {
+        public BreakpointInputBox()
+        {
+            InitializeComponent();
+        }
+
+        private void BreakpointInputBox_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string textToVerify = inputTextBox.Text;
+            int textLength = textToVerify.Length;
+
+            if(textLength == 0 || textLength > 4)
+            {
+                e.Cancel = true;
+
+                MessageBox.Show("Address length must be 1 to 4 hexadecimal characters.");
+            }
+
+            ushort address = 0;
+            bool parseSuccess = ushort.TryParse(textToVerify, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.CurrentCulture, out address);
+
+            if(!parseSuccess)
+            {
+                e.Cancel = true;
+
+                MessageBox.Show("This is not a valid hexadecimal address.");
+            }
+
+
+        }
+    }
+}
