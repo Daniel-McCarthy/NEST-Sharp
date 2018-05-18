d0a2d091e7db0aabe3aa4a5c215b6c188c51a3d7
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-18 17:31:06 -0500

    CPU: Fixed popStackU16 popping bytes from stack in reverse

--- Source/NEST/Classes/CPU.cs
+++ Source/NEST/Classes/CPU.cs
@@ -144,7 +144,7 @@ namespace NEST.Classes
 
         public ushort popStackU16()
         {
-            return (ushort)((popStackU8() << 8) | popStackU8());
+            return (ushort)(popStackU8() | (popStackU8() << 8));
         }
 
         private bool detectADCOverflow(int value, int addition, int sum)
