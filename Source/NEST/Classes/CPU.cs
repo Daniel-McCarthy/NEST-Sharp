a900d7a21664a039ce1eea1a567a596804126f7b
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-19 11:30:53 -0500

    CPU: Fixed flags for Dec memory instruction 0xCE

--- Source/NEST/Classes/CPU.cs
+++ Source/NEST/Classes/CPU.cs
@@ -2021,11 +2021,12 @@ namespace NEST.Classes
 
             ushort address = readImmediateUShort();
             byte value = readCPURam(address);
+            --value;
 
-            writeCPURam(address, --value);
+            writeCPURam(address, value);
 
-            setFlagTo(Zero_Flag, yAddress == 0);
-            setFlagTo(Negative_Flag, (yAddress & 0x80) != 0);
+            setFlagTo(Zero_Flag, value == 0);
+            setFlagTo(Negative_Flag, (value & 0x80) != 0);
 
             //6 Cycles
             mClock += 1;
