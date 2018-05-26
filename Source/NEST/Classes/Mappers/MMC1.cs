3beb225a0e95acc612e8e9de64d847ebc0470f16
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-25 21:10:33 -0500

    MMC1: Added loadCHRRomBank Function

--- Source/NEST/Classes/Mappers/MMC1.cs
+++ Source/NEST/Classes/Mappers/MMC1.cs
@@ -9,5 +9,34 @@ namespace NEST.Classes.Mappers
     class MMC1
     {
 
+        public static void loadChrRomBank(ref Rom romFile, ushort address, byte bankNumber)
+        {
+            //0x0000-0x0FFF Chr Rom Data Bank 1
+            //0x1000-0x1FFF Chr Rom Data Bank 2
+
+            int dataLength = romFile.getExactDataLength();
+            byte[] data = null;
+
+            data = romFile.readBytesFromAddressToEnd(16); //16 in order to skip the INES header
+
+
+            if (data != null)
+            {
+                uint bankAddress = (uint)(0x1000 * (bankNumber));
+                uint prgRomDataAddress = (uint)(0x4000 * romFile.getProgramRomSize()); //Skip trainer if it exists
+
+                for (int i = 0; i < 0x1000; i++)
+                {
+                    ushort writeAddress = (ushort)(address + i);
+                    uint readAddress = (uint)(prgRomDataAddress + bankAddress + i);
+                    if (writeAddress >= 0x0000 && writeAddress <= 0x1FFF && readAddress < data.Length)
+                    {
+                        Core.ppu.writePPURamByte(writeAddress, data[readAddress]);
+                    }
+                }
+            }
+        }
+
+
     }
 }
