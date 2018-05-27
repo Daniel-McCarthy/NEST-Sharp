3d0e224006cc34a48b37562bac8f34c0f0c499da
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-26 20:13:55 -0500

    Mapper: Added function that checks if address matches current mapper registers

--- Source/NEST/Classes/Mappers/Mapper.cs
+++ Source/NEST/Classes/Mappers/Mapper.cs
@@ -9,5 +9,20 @@ namespace NEST.Classes.Mappers
     class Mapper
     {
 
+        public static bool isMapperWriteAddress(ushort address)
+        {
+            if (Core.rom != null)
+            {
+                int mapperSetting = Core.rom.getMapperSetting();
+
+                if (mapperSetting == 1)
+                {
+                    return MMC1.isMapperWriteAddress(address);
+                }
+            }
+
+            return false;
+        }
+
     }
 }
