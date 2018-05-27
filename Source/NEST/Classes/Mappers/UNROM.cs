90ad2bb277ddf9d28b67f0cdd3bcc3eaca7a2f4b
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-26 21:49:43 -0500

    UNROM: Added loadPrgRomBank Function

--- Source/NEST/Classes/Mappers/UNROM.cs
+++ Source/NEST/Classes/Mappers/UNROM.cs
@@ -15,5 +15,34 @@ namespace NEST.Classes.Mappers
         }
 
 
+        public static void loadPrgRomBank(ref Rom romFile, ushort address, byte bankNumber)
+        {
+            //0x8000-0xBFFF, starts with first rom bank
+            //0xC000-0xFFFF, fixed to last rom bank
+
+            int dataLength = romFile.getExactDataLength();
+            byte[] data = null;
+
+            data = romFile.readBytesFromAddressToEnd(16); //16 in order to skip the INES header
+
+
+            if (data != null)
+            {
+                uint bankAddress = (uint)(0x4000 * (bankNumber));
+                uint prgRomDataAddress = (uint)((romFile.getTrainerIncluded()) ? 0x0200 : 0x0000); //Skip trainer if it exists
+
+                for (int i = 0; i < 0x4000; i++)
+                {
+                    ushort writeAddress = (ushort)(address + i);
+                    uint readAddress = (uint)(prgRomDataAddress + bankAddress + i);
+                    if (writeAddress >= 0x8000 && writeAddress <= 0xFFFF && readAddress < data.Length)
+                    {
+                        Core.cpu.directCPURamWrite(writeAddress, data[readAddress]);
+                    }
+                }
+            }
+        }
+
+
     }
 }
