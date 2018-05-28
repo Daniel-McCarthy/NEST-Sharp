c7c4bfb37a8348f37ec92d5ab54513a91848f33c
Dan McCarthy <wererabbbit@yahoo.com>
2018-05-28 15:25:23 -0500

    CNROM: Added writeCNROM Function

--- Source/NEST/Classes/Mappers/CNROM.cs
+++ Source/NEST/Classes/Mappers/CNROM.cs
@@ -15,5 +15,14 @@ namespace NEST.Classes.Mappers
             return address >= 0x8000 && address <= 0xFFFF;
         }
 
+        public static void writeCNROM(ushort address, byte value)
+        {
+            if (address >= 0x8000 && address <= 0xFFFF)
+            {
+                //Select CHR Rom Bank to swap to
+                loadChrRomBank(ref Core.rom, 0x0000, (byte)(value & 0b11));
+            }
+        }
+
     }
 }
