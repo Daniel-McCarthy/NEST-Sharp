4cedd55e7e29f633dd28c7e941a6d58d18a02a3e
Dan McCarthy <wererabbbit@yahoo.com>
2018-06-04 22:23:35 -0500

    MMC3: Added function to load Program Rom Banks

--- Source/NEST/Classes/Mappers/MMC3.cs
+++ Source/NEST/Classes/Mappers/MMC3.cs
@@ -14,5 +14,39 @@ namespace NEST.Classes.Mappers
             return address >= 0x8000 && address <= 0xFFFF;
         }
 
+
+        public static void loadPrgRomBank(ref Rom romFile, ushort address, byte bankNumber)
+        {
+            //0x8000-0x9FFF, switchable rom bank
+            //0xA000-0xBFFF, switchable rom bank
+
+            //0xC000-0xDFFF, fixed to second to last rom bank
+            //0xE000-0xFFFF, fixed to last rom bank
+
+            //The order of the swappable and fixed banks can be swithed.
+
+            int dataLength = romFile.getExactDataLength();
+            byte[] data = null;
+
+            data = romFile.readBytesFromAddressToEnd(16); //16 in order to skip the INES header
+
+
+            if (data != null)
+            {
+                int bankSize = 0x2000;
+                uint bankAddress = (uint)(bankSize * (bankNumber));
+                uint prgRomDataAddress = (uint)((romFile.getTrainerIncluded()) ? 0x0200 : 0x0000); //Skip trainer if it exists
+
+                for (int i = 0; i < bankSize; i++)
+                {
+                    ushort writeAddress = (ushort)(address + i);
+                    uint readAddress = (uint)(prgRomDataAddress + bankAddress + i);
+                    if (writeAddress >= 0x8000 && writeAddress <= 0xFFFF && readAddress < data.Length)
+                    {
+                        Core.cpu.directCPURamWrite(writeAddress, data[readAddress]);
+                    }
+                }
+            }
+        }
     }
 }
