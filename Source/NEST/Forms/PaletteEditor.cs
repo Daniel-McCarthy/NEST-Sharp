b053ce93a0d31b63df9d01861a0d5a6241aafdb4
Dan McCarthy <wererabbbit@yahoo.com>
2018-06-05 20:20:48 -0500

    PaletteEditor: Added pixtureBoxes and selectedPictureBoxIndex Variables
    These keep track of the picture boxes that represent the palette colors.
    There are two many to efficiently refer to them individually. Instead
    they have to be put into a list that can be indexed by number.

--- Source/NEST/Forms/PaletteEditor.cs
+++ Source/NEST/Forms/PaletteEditor.cs
@@ -12,10 +12,14 @@ namespace NEST.Forms
 {
     public partial class PaletteEditor : Form
     {
+        public List<PictureBox> pictureBoxes;
+        private int selectedPictureBoxIndex = -1;
 
         public PaletteEditor()
         {
             InitializeComponent();
+
+            pictureBoxes = new List<PictureBox> { color0PictureBox, color1PictureBox, color2PictureBox, color3PictureBox, color4PictureBox, color5PictureBox, color6PictureBox, color7PictureBox, color8PictureBox, color9PictureBox, color10PictureBox, color11PictureBox, color12PictureBox, color13PictureBox, color14PictureBox, color15PictureBox, color16PictureBox, color17PictureBox, color18PictureBox, color19PictureBox, color20PictureBox, color21PictureBox, color22PictureBox, color23PictureBox, color24PictureBox, color25PictureBox, color26PictureBox, color27PictureBox, color28PictureBox, color29PictureBox, color30PictureBox, color31PictureBox, color32PictureBox, color33PictureBox, color34PictureBox, color35PictureBox, color36PictureBox, color37PictureBox, color38PictureBox, color39PictureBox, color40PictureBox, color41PictureBox, color42PictureBox, color43PictureBox, color44PictureBox, color45PictureBox, color46PictureBox, color47PictureBox, color48PictureBox, color49PictureBox, color50PictureBox, color51PictureBox, color52PictureBox, color53PictureBox, color54PictureBox, color55PictureBox, color56PictureBox, color57PictureBox, color58PictureBox, color59PictureBox, color60PictureBox, color61PictureBox, color62PictureBox, color63PictureBox };
 
         }
 
